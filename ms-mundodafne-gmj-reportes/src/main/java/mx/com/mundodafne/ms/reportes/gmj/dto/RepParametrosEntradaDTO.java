5234ecc65ced9f8fbd0e3844fc004a27cdf16a7c
ccastilloc <cyccov@gmail.com>
2024-11-17 07:51:57 -0600

    ajustes ms

---
+++ ms-mundodafne-gmj-reportes/src/main/java/mx/com/mundodafne/ms/reportes/gmj/dto/RepParametrosEntradaDTO.java
@@ -0,0 +1,17 @@
+package mx.com.mundodafne.ms.reportes.gmj.dto;
+
+import java.util.Map;
+
+public class RepParametrosEntradaDTO {
+	private Map<String, Object> parametros;
+
+	public Map<String, Object> getParametros() {
+		return parametros;
+	}
+
+	public void setParametros(Map<String, Object> parametros) {
+		this.parametros = parametros;
+	}
+	
+	
+}
---
+++ ms-mundodafne-gmj-reportes/src/main/java/mx/com/mundodafne/ms/reportes/gmj/dto/RepSalidaReporteDTO.java
@@ -0,0 +1,20 @@
+package mx.com.mundodafne.ms.reportes.gmj.dto;
+
+public class RepSalidaReporteDTO implements java.io.Serializable {
+	private String base64;
+	private String mensaje;
+	public String getBase64() {
+		return base64;
+	}
+	public void setBase64(String base64) {
+		this.base64 = base64;
+	}
+	public String getMensaje() {
+		return mensaje;
+	}
+	public void setMensaje(String mensaje) {
+		this.mensaje = mensaje;
+	}
+	
+	
+}
