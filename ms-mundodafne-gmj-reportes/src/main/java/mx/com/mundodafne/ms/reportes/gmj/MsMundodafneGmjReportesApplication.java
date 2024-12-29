2c11aedf9034cea2904f5a2202ef0918e465c3d3
ccastilloc <cyccov@gmail.com>
2024-12-29 07:52:17 -0600

    ajustes

--- ms-mundodafne-gmj-reportes/src/main/java/mx/com/mundodafne/ms/reportes/gmj/MsMundodafneGmjReportesApplication.java
+++ ms-mundodafne-gmj-reportes/src/main/java/mx/com/mundodafne/ms/reportes/gmj/MsMundodafneGmjReportesApplication.java
@@ -2,8 +2,9 @@ package mx.com.mundodafne.ms.reportes.gmj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class MsMundodafneGmjReportesApplication {
 
 	public static void main(String[] args) {
