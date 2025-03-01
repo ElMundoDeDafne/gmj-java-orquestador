d7a1a8e2d418671ffa7d6bad292dbbabfdeab5e8
ccastilloc <cyccov@gmail.com>
2025-03-01 09:55:37 -0600

    update

--- ms-mundodafne-gmj-hoja-frontal/src/main/java/mx/com/mundodafne/gmj/hoja/frontal/app/GmjHojaFrontalAppImpl.java
+++ ms-mundodafne-gmj-hoja-frontal/src/main/java/mx/com/mundodafne/gmj/hoja/frontal/app/GmjHojaFrontalAppImpl.java
@@ -2,8 +2,12 @@ package mx.com.mundodafne.gmj.hoja.frontal.app;
 
 import org.springframework.stereotype.Service;
 
+import mx.com.mundodafne.gmj.hoja.frontal.dto.GmjDatosContactoPacienteDTO;
+import mx.com.mundodafne.gmj.hoja.frontal.dto.GmjDomicilioPacienteDTO;
 import mx.com.mundodafne.gmj.hoja.frontal.dto.GmjFrontalRespSalidaDTO;
+import mx.com.mundodafne.gmj.hoja.frontal.dto.GmjHistoriaClinicaDTO;
 import mx.com.mundodafne.gmj.hoja.frontal.dto.GmjHojaFrontalDTO;
+import mx.com.mundodafne.gmj.hoja.frontal.dto.GmjInformacionPacienteDTO;
 
 @Service
 public class GmjHojaFrontalAppImpl implements GmjHojaFrontalApp {
@@ -11,6 +15,13 @@ public class GmjHojaFrontalAppImpl implements GmjHojaFrontalApp {
 	@Override
 	public GmjFrontalRespSalidaDTO registrarInfoBD(GmjHojaFrontalDTO hojaFrontalDTO) {
 		GmjFrontalRespSalidaDTO salida = null;
+		String[] msgs = null;
+		GmjInformacionPacienteDTO infoPx = hojaFrontalDTO.getInformacionPx();
+		GmjDatosContactoPacienteDTO datosContactoPx = hojaFrontalDTO.getDatosContacto();
+		GmjDomicilioPacienteDTO domicilioPx = hojaFrontalDTO.getDomicilioPaciente();
+		GmjHistoriaClinicaDTO historiaClinica =hojaFrontalDTO.getHistoriaClinica();
+		//validaciones de datos
+		
 		
 		return salida;
 
