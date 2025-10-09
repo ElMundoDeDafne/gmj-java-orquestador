e6ff7fc881527ab6bd1ebc7ab7acb258d5460a94
Christian Castillo <cyccov@gmail.com>
2025-10-09 01:16:31 -0600

    insomnio

--- ms-mundodafne-gmj-hoja-frontal/src/main/java/mx/com/mundodafne/gmj/hoja/frontal/app/GmjValdadoresAppImpl.java
+++ ms-mundodafne-gmj-hoja-frontal/src/main/java/mx/com/mundodafne/gmj/hoja/frontal/app/GmjValdadoresAppImpl.java
@@ -5,6 +5,8 @@ package mx.com.mundodafne.gmj.hoja.frontal.app;
 
 import org.springframework.stereotype.Service;
 
+import mx.com.mundodafne.gmj.hoja.frontal.dto.GmjAntHdoFamDTO;
+import mx.com.mundodafne.gmj.hoja.frontal.dto.GmjAntPatologicoDTO;
 import mx.com.mundodafne.gmj.hoja.frontal.dto.GmjDatosContactoPacienteDTO;
 import mx.com.mundodafne.gmj.hoja.frontal.dto.GmjDomicilioPacienteDTO;
 import mx.com.mundodafne.gmj.hoja.frontal.dto.GmjHistoriaClinicaDTO;
@@ -31,6 +33,14 @@ public class GmjValdadoresAppImpl implements GmjValidadoresApp {
 			if(dto.getOcupacion()==null || dto.getOcupacion().isBlank()) {
 				throw new BusinessException("Seleccione ocupacion del paciente.");
 			}
+			
+			if((dto.getDerechoHabiencia() != null && 
+				!dto.getDerechoHabiencia().isBlank()) &&
+				(dto.getNss() != null && 
+				dto.getNss().isEmpty())
+				) {
+				throw new BusinessException("Falta especificar NSS");
+			}
 		}
 	}
 
@@ -46,7 +56,41 @@ public class GmjValdadoresAppImpl implements GmjValidadoresApp {
 
 	@Override
 	public void validarHistoriaClinicaPx(GmjHistoriaClinicaDTO dto) throws BusinessException {
-
+		int total;
+		int i = 0;
+		StringBuilder mensajes = new StringBuilder();
+		GmjAntPatologicoDTO antPat;
+		GmjAntHdoFamDTO antHdoFam;
+		if(dto!=null) {
+			/*validando los antecedentes patologicos del paciente*/
+			total = dto.getAntecPatologicos().length;
+			while(i < total) {
+				antPat = dto.getAntecPatologicos()[i];
+				if("otro".equalsIgnoreCase(antPat.getNombreEnfermedad()) && antPat.getOtraEnfermedad().isBlank() || antPat.getOtraEnfermedad().isEmpty()) {
+					mensajes.append("Falta definir nombre de enfermedad|");
+				}
+				
+				if(antPat!=null && antPat.getTiempoEvolucion().isBlank() || antPat.getTiempoEvolucion().isEmpty()) {
+					mensajes.append("Falta definir tiempo de evolucion ["+antPat.getNombreEnfermedad()+"]|");
+				}
+				i++;
+			}
+			
+			i=0;
+			total = dto.getAntecedentesHdoFamiliares().length;
+			//hdo familiares
+			while(i<total) {
+				antHdoFam = dto.getAntecedentesHdoFamiliares()[i];
+				if(antHdoFam.getMorbilidadFamiliar() != null && antHdoFam.getMorbilidadFamiliar().isBlank() || antHdoFam.getMorbilidadFamiliar().isEmpty()) {
+					mensajes.append("Falta definir morbilidad familiar ["+antHdoFam.getFamiliar()+"-"+antHdoFam.getParentesco()+"]|");
+				}
+				i++;
+			}
+			
+			if(!mensajes.toString().isEmpty()) {
+				throw new BusinessException(mensajes.toString());
+			}
+		}
 	}
 
 }
